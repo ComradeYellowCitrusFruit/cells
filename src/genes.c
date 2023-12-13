61bfbcfa92883195d511f138341a88d5aa55f6a4
Teresa <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-12-13 17:02:44 +0000

    Added function to handle inheritance
    Added the function to duplicate dna, and possibly mutate dna.

---
+++ src/genes.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "include/genetics.h"
+#include "include/rng.h"
+
+extern generator_handle main_rng;
+
+/* handles inheritance + mutation */
+void duplicate_genes(gene_t parent[4], gene_t child[4]) {
+	uint16_t dice_roll_a = gen16(main_rng);
+	memcpy(child, parent, 16);
+
+	if(dice_roll_a == 0xabcd) {
+		child[gen8(main_rng) % 4] ^= 1 << gen8(main_rng) % 8;
+
+		if(gen64(main_rng) & 0xffffffffffff == 0)
+			parent[gen8(main_rng) % 4] ^= 1 << gen8(main_rng) % 8;
+	} else if(dice_roll_a == 0xef00) {
+		parent[gen8(main_rng) % 4] ^= 1 << gen8(main_rng) % 8;
+		
+		if(gen64(main_rng) & 0xffffffffffff == 0)
+			child[gen8(main_rng) % 4] ^= 1 << gen8(main_rng) % 8;
+	}
+}
